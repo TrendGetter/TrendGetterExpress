cd4c308e5dc32f8cf14756180ef7b7f6716f0703
Hunter Lombard <hwlombard@gmail.com>
2017-04-02 17:29:34 -0400

    finish express get requests

--- controllers/youtubeApi/controller.js
+++ controllers/youtubeApi/controller.js
@@ -3,13 +3,14 @@ const Youtube = require('../../services/youtube.js');
 let controller = {};
 
 controller.getYoutubeResults = (req, res) => {
-  console.log('iygihgoygiuygiuygiuy');
+  // console.log('iygihgoygiuygiuygiuy');
   Youtube
     .getYoutubeResults()
     .then(r => r.json())
     .then(data => {
+      // console.log(data);
       let youtubeResult = data.items[0].snippet.title
-      
+
       console.log(youtubeResult);
     });
 };
--- services/youtube.js
+++ services/youtube.js
@@ -1,8 +1,9 @@
 const fetch       = require('node-fetch');
 const Youtube    = {};
+const APP_ID      = process.env.APP_ID;
 
 Youtube.getYoutubeResults = () => {
-  return fetch(`https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=10&key=AIzaSyBpKwgs4rwLN7V_42KpgQwE3xz0NA56yRM`);
+  return fetch(`https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=10&key=${APP_ID}`);
 };
 
 module.exports = Youtube;
